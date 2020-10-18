6d37a38a40d91d71da16d161da47fe47f07c3b27
Esteban Raffo <e.fraffo@gmail.com>
2020-10-18 14:09:27 -0300

    18/10

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import Search from "../containers/Search";
-import Menu from "./Menu";
+import Header from "./Header";
+import Banner from "./Banner";
+import Filters from "./Filters";
+import Sorter from "./Sorter";
 import Home from "./Home";
-import NewsBySearch from "./NewsBySearch";
+import History from "./History";
+
 
 const Main = () => {
   return (
     <div>
-      <Search />
-      <Menu />
+      <Header/>
+      <Banner/>
+      <Filters/>
+      <Sorter/>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/search/:searchTerm" component={NewsBySearch} />
+        <Route path="/user/history" component={History} />
       </Switch>
     </div>
   );
